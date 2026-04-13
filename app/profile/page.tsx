9cfa7fe086ea5f9610eb1d1218fa7a95f9735717
vikram <vikramyreddy@gmail.com>
2026-04-13 12:33:17 +0530

    changes

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import ProtectedPage from '@/components/ProtectedPage';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
